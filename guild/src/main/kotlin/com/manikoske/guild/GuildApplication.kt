8f9e39cba7c921206ae5134aac1d3d59e6b127f0
Marek Kovacik <marek.kovacik@nike.sk>
2024-12-23 23:26:13 +0100

    first commit

---
+++ guild/src/main/kotlin/com/manikoske/guild/GuildApplication.kt
@@ -0,0 +1,11 @@
+package com.manikoske.guild
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GuildApplication
+
+fun main(args: Array<String>) {
+	runApplication<GuildApplication>(*args)
+}
