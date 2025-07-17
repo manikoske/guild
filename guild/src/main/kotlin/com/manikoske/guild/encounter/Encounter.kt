2d872201e7a0fa909b11ba238d89734a68787e03
marek.kovacik <marek.kovacik@nike.sk>
2025-07-17 14:51:52 +0200

    logging changes

--- guild/src/main/kotlin/com/manikoske/guild/encounter/Encounter.kt
+++ guild/src/main/kotlin/com/manikoske/guild/encounter/Encounter.kt
@@ -14,7 +14,7 @@ class Encounter(
     ): State {
         // Create initial character states
         val initialState = State(
-            updatedCharacterStates = 
+            startingCharacterStates =
                 attackers.map {
                     CharacterState(
                         character = it,
@@ -35,18 +35,20 @@ class Encounter(
                         effects = CharacterState.noEffects(),
                     )
                 },
+            updatedCharacterStates = listOf(),
             rounds = listOf()
         )
 
         // Run the simulation without logging
         val result = (1..20).fold(initialState) { encounterState, roundSequence ->
-            val round = Round(sequence = roundSequence, characterStates = encounterState.updatedCharacterStates)
+
+            val round = Round(sequence = roundSequence, characterStates = encounterState.startingCharacterStates)
                 .simulate(battleground)
 
             if (round.turns.isEmpty()) {
                 encounterState
             } else {
-                State(
+                encounterState.copy(
                     updatedCharacterStates = round.updatedCharacterStates,
                     rounds = encounterState.rounds + round
                 )
@@ -57,6 +59,7 @@ class Encounter(
     }
 
     data class State(
+        val startingCharacterStates: List<CharacterState>,
         val updatedCharacterStates : List<CharacterState>,
         val rounds : List<Round.State>
     )
